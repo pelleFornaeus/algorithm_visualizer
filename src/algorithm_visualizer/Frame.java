6191692c2ed23730b8bfecf6d10fd68d26f421c0
Per Fornaeus <pellefornaeus@gmail.com>
2019-11-07 11:06:20 -0800

    Added drop down menu

--- src/algorithm_visualizer/Frame.java
+++ src/algorithm_visualizer/Frame.java
@@ -1,7 +1,24 @@
 package algorithm_visualizer;
 
-public class Frame {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Frame extends JFrame implements ActionListener{
+	public Frame() {
+		this.setTitle("Algorithm Visualizer");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	public JComboBox dropDownMenu() {
+		String[] algorithms = {"Bubble Sort", "Merge Sort", "Quick Sort"};
+		JComboBox algorithmList = new JComboBox(algorithms);
+		algorithmList.setSelectedIndex(0);
+		algorithmList.addActionListener(this);
+		return algorithmList;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 }
